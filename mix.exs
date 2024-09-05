4633f80b0678e2636bd5a4aabf351ac53d7fa1d6
Zac Hunsaker <zkh3@georgetown.edu>
2024-09-05 16:04:49 -0500

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EntityFingerprint.MixProject do
   def project do
     [
       app: :entityfingerprint,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
